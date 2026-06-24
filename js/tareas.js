258fe3dfdfd6c7c0ef1152c383aa634d43c5fb70
eli0-1trujillo <eliinfante327@gmail.com>
2026-06-24 17:57:24 -0500

    Add files via upload

---
+++ js/tareas.js
@@ -0,0 +1,184 @@
+let tareas =
+JSON.parse(localStorage.getItem("tareas")) || [];
+
+function guardarLocalStorage(){
+
+    localStorage.setItem(
+        "tareas",
+        JSON.stringify(tareas)
+    );
+
+}
+
+function guardarTarea(){
+
+    let titulo =
+    document.getElementById("titulo").value;
+
+    if(titulo.trim() === ""){
+
+        alert("Ingrese un título");
+        return;
+
+    }
+
+    let descripcion =
+    document.getElementById("descripcion").value;
+
+    let prioridad =
+    document.getElementById("prioridad").value;
+
+    let fecha =
+    document.getElementById("fechaLimite").value;
+
+    tareas.push({
+
+        titulo: titulo,
+        descripcion: descripcion,
+        prioridad: prioridad,
+        fecha: fecha,
+        completada: false
+
+    });
+
+    guardarLocalStorage();
+
+    renderizarTareas();
+
+    document.getElementById("titulo").value = "";
+    document.getElementById("descripcion").value = "";
+    document.getElementById("fechaLimite").value = "";
+
+    cerrarModal();
+}
+
+function cambiarEstado(indice){
+
+    tareas[indice].completada =
+    !tareas[indice].completada;
+
+    guardarLocalStorage();
+
+    renderizarTareas();
+}
+
+function eliminar(indice){
+
+    if(confirm("¿Eliminar tarea?")){
+
+        tareas.splice(indice,1);
+
+        guardarLocalStorage();
+
+        renderizarTareas();
+    }
+
+}
+
+function actualizarDashboard(){
+
+    let pendientes =
+    tareas.filter(t => !t.completada).length;
+
+    let completadas =
+    tareas.filter(t => t.completada).length;
+
+    document.getElementById("total").textContent =
+    tareas.length;
+
+    document.getElementById("pendientes").textContent =
+    pendientes;
+
+    document.getElementById("completadas").textContent =
+    completadas;
+}
+
+function renderizarTareas(){
+
+    document.getElementById("colTodas").innerHTML = "";
+    document.getElementById("colPendientes").innerHTML = "";
+    document.getElementById("colCompletadas").innerHTML = "";
+
+    tareas.forEach((tarea,index)=>{
+
+        let html = `
+        <div class="tarea">
+
+            <input
+            type="checkbox"
+            ${tarea.completada ? "checked" : ""}
+            onchange="cambiarEstado(${index})">
+
+            <strong>${tarea.titulo}</strong>
+
+            <p>${tarea.descripcion}</p>
+
+            <small>
+                Prioridad: ${tarea.prioridad}
+            </small>
+            <br>
+
+            <small>
+                Fecha: ${tarea.fecha}
+            </small>
+
+            <div class="acciones">
+
+                <button onclick="eliminar(${index})">
+                    Eliminar
+                </button>
+
+            </div>
+
+        </div>
+        `;
+
+        document.getElementById("colTodas")
+        .innerHTML += html;
+
+        if(tarea.completada){
+
+            document.getElementById("colCompletadas")
+            .innerHTML += html;
+
+        }else{
+
+            document.getElementById("colPendientes")
+            .innerHTML += html;
+
+        }
+
+    });
+
+    actualizarDashboard();
+
+    buscarTarea();
+}
+
+function buscarTarea(){
+
+    let texto =
+    document.getElementById("buscar")
+    .value
+    .toLowerCase();
+
+    document
+    .querySelectorAll(".tarea")
+    .forEach(tarea=>{
+
+        tarea.style.display =
+        tarea.innerText
+        .toLowerCase()
+        .includes(texto)
+        ? "block"
+        : "none";
+
+    });
+
+}
+
+window.onload = function(){
+
+    renderizarTareas();
+
+}
